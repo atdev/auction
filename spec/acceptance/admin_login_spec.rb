10732abdc6d1c9679ce45de8d8efc717f7151ecc
alextim <atimdev@gmail.com>
2014-01-21 00:16:57 +0400

    admin loggin acceptance refactoring

--- spec/acceptance/admin_login_spec.rb
+++ spec/acceptance/admin_login_spec.rb
@@ -18,19 +18,14 @@ feature "Admin logging in to admin area", %q{
     end
 
     scenario "Admin saccessfully logged into admin area" do
-      visit  new_user_session_path
+      visit new_user_session_path
       sign_in_with 'admin@test.com', '12345678'
-      #fill_in "user[email]", with: 'admin@test.com'
-      #fill_in "user[password]", with: '12345678'
-      #click_on "Sign in"
       page.should have_content "Signed in successfully"
     end
 
     scenario "User fill in wrong credentials" do
       visit new_user_session_path
-      fill_in "user[email]", with: 'admin@test.com'
-      fill_in "user[password]", with: 'wrong'
-      click_on "Sign in"
+      sign_in_with 'admin@test.com', 'wrong'
       current_path.should == new_user_session_path
       page.should have_content "Invalid email or password"
     end
@@ -39,9 +34,7 @@ feature "Admin logging in to admin area", %q{
   scenario "Non-admin tries to get access to admin area" do
     User.create!(email: 'user@test.com', password: '12345678', password_confirmation: '12345678', is_admin: false)
     visit new_user_session_path
-    fill_in "user[email]", with: 'user@test.com'
-    fill_in "user[password]", with: '12345678'
-    click_on "Sign in"
+    sign_in_with 'admin@test.com', '12345678'
     visit admin_products_path
     current_path.should == root_path
     page.should have_content "You must be logged in as admin to access this section"
