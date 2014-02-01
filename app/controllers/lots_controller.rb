792a6a8fd4b4d6803c24d248fe1d1101da4cb8ef
alextim <atimdev@gmail.com>
2014-02-01 09:18:51 +0400

    added view show for a lot, raw version

--- app/controllers/lots_controller.rb
+++ app/controllers/lots_controller.rb
@@ -1,9 +1,14 @@
 class LotsController < ApplicationController
+  
   def index
     @lots= Lot.order('created_at DESC').page(params[:page]).per_page(6)
   end
 
   def show
+    @lot = Lot.find(params[:id])
   end
+
+
+
 
 end
