a55a17151aaec86165a9948a63de922c061e4dec
alextim <atimdev@gmail.com>
2014-02-08 05:33:47 +0400

    redirect to sign in page if user is a guest

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,14 +1,32 @@
 class BetsController < ApplicationController
 
   def create
-    lot_id = params[:lot_id]
-    user_id = current_user.id
-    @bet = Bet.new(lot_id: lot_id, user_id: user_id)
-    if @bet.save
-      respond_to do |format|
-        format.js { }
+    unless user_signed_in?
+      redirect_guest
+    else
+      lot_id = params[:lot_id]
+      user_id = current_user.id
+      @bet = Bet.new(lot_id: lot_id, user_id: user_id)
+      if @bet.save
+        respond_to do |format|
+          format.js { }
+        end
       end
     end
   end
+
+  private
+    def redirect_guest
+      unless user_signed_in?
+        flash[:sign_in_make_bet] = "User should sign in to make a bet!"
+        respond_to do |format|
+          format.js { js_redirect }
+        end
+      end
+    end
+
+    def js_redirect
+      render js: %(window.location.href='#{new_user_session_path}')
+    end
 
 end
